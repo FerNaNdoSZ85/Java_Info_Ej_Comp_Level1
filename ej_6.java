953e5f8e202f6c8668a88e7db0a370e11c902b96
FerNaNdo-InfoC4 <fernando.escobar.fi1985@gmail.com>
2021-10-26 21:30:50 -0300

    completado

--- ej_6.java
+++ ej_6.java
@@ -9,15 +9,11 @@ public class ej_6 {
         int potencia = 1;
         if (base == 0) {
         System.out.println(base+ " elevado a " + exponente + "= 0");}
-    else if (exponente == 0) {
+        else if (exponente == 0) {
         System.out.println(base + " elevado a " + exponente + "= 1");
-    }
-    else {
+        }
+        else {
         for (int i = 1; i <= exponente; i++){
             potencia = potencia* base;}
             System.out.println(base + " elevado a " + exponente + " = " + potencia);
-    }
-
-    }
-                
-            }
+        }}}
